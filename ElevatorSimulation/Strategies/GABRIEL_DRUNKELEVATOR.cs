32ef0e13a540ef0d2221e43166c50f3cbd9394b1
Robert Haken <haken@havit.cz>
2025-11-19 23:06:54 +0100

    "fix"

--- ElevatorSimulation/Strategies/GABRIEL_DRUNKELEVATOR.cs
+++ ElevatorSimulation/Strategies/GABRIEL_DRUNKELEVATOR.cs
@@ -1,27 +1,25 @@
-﻿using System.Security.Cryptography;
+﻿namespace ElevatorSimulation.Strategies;
 
-namespace ElevatorSimulation.Strategies;
-
-public class MojeStrategieStrategy : IElevatorStrategy
+public class MojeStrategieStrategy //: IElevatorStrategy
 {
-    public MoveResult DecideNextMove(ElevatorSystem elevator)
-    {
-        //vodka.cs
+	public MoveResult DecideNextMove(ElevatorSystem elevator)
+	{
+		//vodka.cs
 
-        Random rnd = new Random();
-        int elevator_mood = rnd.Next(-1, 1);
+		Random rnd = new Random();
+		int elevator_mood = rnd.Next(-1, 1);
 
-        if (elevator_mood == -1)
-        {
-            return MoveResult.MoveDown;
-        }
-        else if (elevator_mood == 1)
-        {
-            return MoveResult.MoveUp;
-        }
-        else
-        {
-            return MoveResult.OpenDoors;
-        }
-    }
+		if (elevator_mood == -1)
+		{
+			return MoveResult.MoveDown;
+		}
+		else if (elevator_mood == 1)
+		{
+			return MoveResult.MoveUp;
+		}
+		else
+		{
+			return MoveResult.OpenDoors;
+		}
+	}
 }
